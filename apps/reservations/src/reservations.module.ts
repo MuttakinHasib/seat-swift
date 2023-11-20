a90bc886f10a283d478616f72aeef3341efce1db
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2023-11-20 17:04:33 +0600

    feat: :hammer: static model definition

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
 import { ConfigModule, DatabaseModule } from '@app/common';
+import { Reservation, ReservationSchema } from './models/reservation.schema';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule],
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: Reservation.name, schema: ReservationSchema },
+    ]),
+    ConfigModule,
+  ],
   controllers: [ReservationsController],
   providers: [ReservationsService],
 })
--- apps/reservations/src/reservations.repository.ts
+++ apps/reservations/src/reservations.repository.ts
@@ -2,12 +2,15 @@ import { AbstractRepository } from '@app/common';
 import { Injectable, Logger } from '@nestjs/common';
 import { Reservation } from './models/reservation.schema';
 import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class ReservationsRepository extends AbstractRepository<Reservation> {
   protected readonly logger = new Logger(ReservationsRepository.name);
 
-  constructor(protected readonly reservationModel: Model<Reservation>) {
+  constructor(
+    @InjectModel(Reservation.name) reservationModel: Model<Reservation>,
+  ) {
     super(reservationModel);
   }
 }
